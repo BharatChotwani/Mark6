d6cc53746c29a50c284b3312ec732cb1a779c2a8
Bharat <bchotwani3@gmail.com>
2021-06-07 17:20:25 +0530

    added eventlistener

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 var btnTranslate = document.querySelector(".btn-first")
 var txtInput = document.querySelector("#txt-input")
 var txtOutput = document.querySelector(".txt-output")
+
+function clickHandler {
+    var inputText = txtInput.value
+
+}
+
+btnTranslate.addEventListener("click", clickHandler)
