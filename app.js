2366a9e7c9865ed7fb0f195d388decafc8f86baf
Bharat <bchotwani3@gmail.com>
2021-06-07 17:28:23 +0530

    fix

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var btnTranslate = document.querySelector(".btn-first")
 var txtInput = document.querySelector("#txt-input")
 var txtOutput = document.querySelector(".txt-output")
 
-function clickHandler {
+function clickHandler(){
     var inputText = txtInput.value
+    console.log(inputText)
 
 }
 
