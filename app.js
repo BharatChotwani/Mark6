6a9dfeffefb5b8b0718e66f66d887f3276d87636
Bharat <bchotwani3@gmail.com>
2021-06-07 18:22:06 +0530

    fetch call

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ var btnTranslate = document.querySelector(".btn-first")
 var txtInput = document.querySelector("#txt-input")
 var txtOutput = document.querySelector(".txt-output")
 
-function clickHandler(){
-    var inputText = txtInput.value
-    console.log(inputText)
+var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
+function getTranslationURL(text){
+    return serverUrl + "?" + "text=" + text
+}
+
+function clickHandler(){
+    var inputText = txtInput.value  // taking input
+    
+    // calling server for processing
+     fetch(getTranslationURL(inputText))
+     .then (response => response.json())
+     .then (json => {
+         var translateText = json.contents.translated;
+         txtOutput.innerText = translateText; // output
+     })
 }
 
 btnTranslate.addEventListener("click", clickHandler)
